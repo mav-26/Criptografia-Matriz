fed3a6c09806a25a154ee64902fcdad32fb23170
caleg <caleg@DESKTOP-TBL9EH4>
2023-05-16 22:33:19 -0300

    Adicionar arquivos de projeto.

---
+++ Matriz.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QubrarCriptografiaMatriz
+{
+    internal class Matriz
+    {
+        public int Colunas = 0;
+        public int[,] M = new int[2, 100];
+        public int[,] Chave = new int[2, 2];
+
+        public string Frases()
+        {
+            string[,] VetFrase = new string[2, 100];
+            string Frase = " ";
+            string FraseCompleta = "";
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < Colunas; j++)
+                {
+                    switch (M[i, j])
+                    {
+                        case 0: Frase = " "; break;
+                        case 1: Frase = "A"; break;
+                        case 2: Frase = "B"; break;
+                        case 3: Frase = "C"; break;
+                        case 4: Frase = "D"; break;
+                        case 5: Frase = "E"; break;
+                        case 6: Frase = "F"; break;
+                        case 7: Frase = "G"; break;
+                        case 8: Frase = "H"; break;
+                        case 9: Frase = "I"; break;
+                        case 10: Frase = "J"; break;
+                        case 11: Frase = "K"; break;
+                        case 12: Frase = "L"; break;
+                        case 13: Frase = "M"; break;
+                        case 14: Frase = "N"; break;
+                        case 15: Frase = "O"; break;
+                        case 16: Frase = "P"; break;
+                        case 17: Frase = "Q"; break;
+                        case 18: Frase = "R"; break;
+                        case 19: Frase = "S"; break;
+                        case 20: Frase = "T"; break;
+                        case 21: Frase = "U"; break;
+                        case 22: Frase = "V"; break;
+                        case 23: Frase = "W"; break;
+                        case 24: Frase = "X"; break;
+                        case 25: Frase = "Y"; break;
+                        case 26: Frase = "Z"; break;
+                        case 27: Frase = "Á"; break;
+                        case 28: Frase = "É"; break;
+                        case 29: Frase = "Í"; break;
+                        case 30: Frase = "Ó"; break;
+                        case 31: Frase = "Ú"; break;
+                        case 32: Frase = "À"; break;
+                        case 33: Frase = "Â"; break;
+                        case 34: Frase = "Ê"; break;
+                        case 35: Frase = "Ô"; break;
+                        case 36: Frase = "Ã"; break;
+                        case 37: Frase = "Õ"; break;
+                        case 38: Frase = "Ç"; break;
+                        case 39: Frase = "."; break;
+                        case 40: Frase = ","; break;
+                        case 41: Frase = ":"; break;
+                        case 42: Frase = "?"; break;
+                        case 43: Frase = "!"; break;
+                    }
+
+                    VetFrase[i, j] = Frase;
+                }
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < Colunas; j++)
+                {
+                    FraseCompleta += VetFrase[i, j];
+                }
+            }
+            return FraseCompleta;
+        }
+
+        public int[,] MultiplicarMatrizes(int[,] chave, int[,] MatrizCriptografado, int Coluna)
+        {
+            int chaveLinhas = 2;
+            int chaveColunas = 2;
+            int mColunas = Coluna;
+
+            int[,] resultado = new int[chaveLinhas, mColunas];
+
+            for (int i = 0; i < chaveLinhas; i++)
+            {
+                for (int j = 0; j < mColunas; j++)
+                {
+                    int soma = 0;
+                    for (int k = 0; k < chaveColunas; k++)
+                    {
+                        soma += chave[i, k] * MatrizCriptografado[k, j];
+                    }
+                    resultado[i, j] = soma;
+                }
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QubrarCriptografiaMatriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matriz x = new Matriz();
+
+            string Primeira = "";
+            string Segunda = "";
+            int Colunas = 0;
+            int indice = 0;
+            int[,] chave = new int[2, 2];
+
+            Console.WriteLine("1 - Descriptografar matriz");
+            Console.WriteLine("2 - Obter Frase");
+            Console.WriteLine("3 - Para sair do programa");
+            indice = int.Parse(Console.ReadLine());
+
+            while (indice != 3)
+            {
+                Console.Clear();
+                switch (indice)
+                {
+                    case 1:
+                        Primeira = "";
+                        Segunda = "";
+                        Colunas = 0;
+
+                        Console.WriteLine("Matriz Chave:");
+                        for (int i = 0; i < 2; i++)
+                        {
+                            for (int j = 0; j < 2; j++)
+                            {
+                                chave[i, j] = int.Parse(Console.ReadLine());
+                                Console.Clear();
+                                if (i == 0)
+                                {
+                                    Primeira += chave[i, j].ToString() + " ";
+                                    Console.Write(Primeira);
+                                }
+                                else
+                                {
+                                    Segunda += chave[i, j].ToString() + " ";
+                                    Console.WriteLine(Primeira);
+                                    Console.Write(Segunda);
+                                }
+                            }
+                        }
+                        Console.Clear();
+
+                        Console.Write("Numero de Colunas da matriz criptografada: ");
+                        Colunas = int.Parse(Console.ReadLine());
+                        int[,] Criptografado = new int[2, Colunas];
+                        Console.Clear();
+
+                        Console.WriteLine("Matriz Criptografada:");
+                        Primeira = "";
+                        Segunda = "";
+                        for (int i = 0; i < 2; i++)
+                        {
+                            for (int j = 0; j < Colunas; j++)
+                            {
+                                Criptografado[i, j] = int.Parse(Console.ReadLine());
+                                Console.Clear();
+                                if (i == 0)
+                                {
+                                    Primeira += Criptografado[i, j].ToString() + " ";
+                                    Console.Write(Primeira);
+                                }
+                                else
+                                {
+                                    Segunda += Criptografado[i, j].ToString() + " ";
+                                    Console.WriteLine(Primeira);
+                                    Console.Write(Segunda);
+                                }
+                            }
+                        }
+
+                        int[,] resultado = x.MultiplicarMatrizes(chave, Criptografado, Colunas);
+                        Primeira = "";
+                        Segunda = "";
+                        for (int i = 0; i < 2; i++)
+                        {
+                            for (int j = 0; j < Colunas; j++)
+                            {
+                                Console.Clear();
+                                if (i == 0)
+                                {
+                                    Primeira += resultado[i, j].ToString() + " ";
+                                    Console.Write(Primeira);
+                                }
+                                else
+                                {
+                                    Segunda += resultado[i, j].ToString() + " ";
+                                    Console.WriteLine(Primeira);
+                                    Console.Write(Segunda);
+                                }
+                            }
+                        }
+                        Console.WriteLine("Pressione qualquer tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    
+                    case 2:
+                        Primeira = "";
+                        Segunda = "";
+                        Colunas = 0;
+
+                        Console.Write("Número de colunas: ");
+                        Colunas = int.Parse(Console.ReadLine());
+                        x.Colunas = Colunas;
+                        Console.Clear();
+
+                        for (int i = 0; i < 2; i++)
+                        {
+                            for (int j = 0; j < Colunas; j++)
+                            {                             
+                                x.M[i, j] = int.Parse(Console.ReadLine());
+                                Console.Clear();
+                                if (i == 0)
+                                {
+                                    Primeira += x.M[i, j].ToString() + " ";
+                                    Console.Write(Primeira);
+                                }
+                                else
+                                {
+                                    Segunda += x.M[i, j].ToString() + " ";
+                                    Console.WriteLine(Primeira);
+                                    Console.Write(Segunda);
+                                }
+                            }
+                        }
+
+                        Console.WriteLine("");
+                        Console.WriteLine(x.Frases());
+                        Console.WriteLine("Pressione qualquer tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                }
+                Console.Clear();
+
+                Console.WriteLine("1 - Descriptografar matriz");
+                Console.WriteLine("2 - Obter Frase");
+                indice = int.Parse(Console.ReadLine());
+            }        
+        }
+    }
+}
